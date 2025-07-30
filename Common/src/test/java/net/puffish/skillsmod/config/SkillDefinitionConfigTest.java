909810f4f62b9a5e43a6c9f76f89a214a2433e11
Blue Lotus Coding <62197849+DeveloperNoShinigami@users.noreply.github.com>
2025-07-30 11:40:54 -0500

    Remove parent from skill definitions

--- Common/src/test/java/net/puffish/skillsmod/config/SkillDefinitionConfigTest.java
+++ Common/src/test/java/net/puffish/skillsmod/config/SkillDefinitionConfigTest.java
@@ -5,7 +5,6 @@ import net.puffish.skillsmod.api.config.ConfigContext;
 import net.puffish.skillsmod.api.json.JsonElement;
 import net.puffish.skillsmod.api.json.JsonPath;
 import net.puffish.skillsmod.config.skill.SkillDefinitionConfig;
-import net.puffish.skillsmod.config.skill.SkillDefinitionsConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -43,38 +42,4 @@ public class SkillDefinitionConfigTest {
         Assertions.assertTrue(config.mergeDescription());
     }
 
-    @Test
-    public void testParentDescriptionMerge() {
-        String json = """
-                {
-                  \"parent\": {
-                    \"title\": {\"text\": \"Parent\"},
-                    \"icon\": {\"type\": \"texture\", \"data\": {\"texture\": \"minecraft:stone\"}},
-                    \"descriptions\": [\"A\"],
-                    \"extra_descriptions\": [\"EA\"]
-                  },
-                  \"child\": {
-                    \"title\": {\"text\": \"Child\"},
-                    \"icon\": {\"type\": \"texture\", \"data\": {\"texture\": \"minecraft:stone\"}},
-                    \"parent\": \"parent\",
-                    \"merge_description\": true,
-                    \"descriptions\": [\"B\"],
-                    \"extra_descriptions\": [\"EB\"]
-                  }
-                }
-                """;
-
-        var element = JsonElement.parseString(json, JsonPath.create("test"))
-                .getSuccess().orElseThrow();
-
-        var result = SkillDefinitionsConfig.parse(element, new DummyContext());
-        Assertions.assertTrue(result.getSuccess().isPresent(),
-                result.getFailure().map(Object::toString).orElse("Unexpected failure"));
-        var defs = result.getSuccess().orElseThrow();
-        var child = defs.getById("child").orElseThrow();
-        Assertions.assertEquals(2, child.descriptions().size());
-        Assertions.assertEquals(2, child.extraDescriptions().size());
-        Assertions.assertEquals("A", child.descriptions().get(0).getString());
-        Assertions.assertEquals("EA", child.extraDescriptions().get(0).getString());
-    }
 }

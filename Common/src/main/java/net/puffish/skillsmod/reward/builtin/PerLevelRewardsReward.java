a808bc0ea4e0c6a5724c4dac4a772eda7fabccf8
Blue Lotus Coding <62197849+DeveloperNoShinigami@users.noreply.github.com>
2025-07-30 23:53:18 -0500

    Remove parent inheritance from skill definitions

--- Common/src/main/java/net/puffish/skillsmod/reward/builtin/PerLevelRewardsReward.java
+++ Common/src/main/java/net/puffish/skillsmod/reward/builtin/PerLevelRewardsReward.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.Map;
 
 public class PerLevelRewardsReward implements Reward {
-	public static final Identifier ID = SkillsMod.createIdentifier("per_level_rewards");
+    public static final Identifier ID = SkillsMod.createIdentifier("per_level_rewards");
 
     private final Map<Integer, List<SkillRewardConfig>> levelRewards;
     private final String skillId;
@@ -49,110 +49,100 @@ public class PerLevelRewardsReward implements Reward {
         return pointsPerLevel;
     }
 
-	public static void register() {
-	SkillsAPI.registerReward(ID, PerLevelRewardsReward::parse);
-	}
+    public static void register() {
+        SkillsAPI.registerReward(ID, PerLevelRewardsReward::parse);
+    }
 
-	private static Result<PerLevelRewardsReward, Problem> parse(RewardConfigContext context) {
-	return context.getData()
-		.andThen(JsonElement::getAsObject)
-		.andThen(LegacyUtils.wrapNoUnused(obj -> parse(obj, context), context));
-	}
+    private static Result<PerLevelRewardsReward, Problem> parse(RewardConfigContext context) {
+        return context.getData()
+                .andThen(JsonElement::getAsObject)
+                .andThen(LegacyUtils.wrapNoUnused(obj -> parse(obj, context), context));
+    }
 
-	private static Result<PerLevelRewardsReward, Problem> parse(JsonObject rootObject, ConfigContext context) {
-	var problems = new ArrayList<Problem>();
+    private static Result<PerLevelRewardsReward, Problem> parse(JsonObject rootObject, ConfigContext context) {
+        var problems = new ArrayList<Problem>();
 
-	var optLevelsMap = rootObject.getObject("levels")
-		.andThen(obj -> obj.getAsMap((key, element) ->
-			element.getAsArray()
-				.andThen(arr -> arr.getAsList((i, e) -> SkillRewardConfig.parse(e, context))
-					.mapFailure(Problem::combine))
-		).mapFailure(map -> Problem.combine(map.values())))
-		.ifFailure(problems::add)
-		.getSuccess();
+        var optLevelsMap = rootObject.getObject("levels")
+                .andThen(obj -> obj.getAsMap((key, element) ->
+                        element.getAsArray()
+                                .andThen(arr -> arr.getAsList((i, e) -> SkillRewardConfig.parse(e, context))
+                                        .mapFailure(Problem::combine))
+                ).mapFailure(map -> Problem.combine(map.values())))
+                .ifFailure(problems::add)
+                .getSuccess();
 
-	var levelsPath = rootObject.getPath().getObject("levels");
-	var levelRewards = new HashMap<Integer, List<SkillRewardConfig>>();
-	optLevelsMap.ifPresent(map -> {
-		for (var entry : map.entrySet()) {
-		try {
-			var level = Integer.parseInt(entry.getKey());
-			levelRewards.put(level, entry.getValue());
-		} catch (NumberFormatException e) {
-			problems.add(levelsPath.getObject(entry.getKey()).createProblem("Expected an integer"));
-		}
-		}
-	});
+        var levelsPath = rootObject.getPath().getObject("levels");
+        var levelRewards = new HashMap<Integer, List<SkillRewardConfig>>();
+        optLevelsMap.ifPresent(map -> {
+            for (var entry : map.entrySet()) {
+                try {
+                    var level = Integer.parseInt(entry.getKey());
+                    levelRewards.put(level, entry.getValue());
+                } catch (NumberFormatException e) {
+                    problems.add(levelsPath.getObject(entry.getKey()).createProblem("Expected an integer"));
+                }
+            }
+        });
 
-	// Access optional fields and validate values
-	rootObject.get("skill_id");
-	
-	var optMaxLevel = rootObject.get("max_level")
-	.getSuccess() // ignore failure because this property is optional
-	.flatMap(element -> element.getAsInt()
-	.ifFailure(problems::add)
-	.getSuccess());
-	optMaxLevel.ifPresent(maxLevel -> {
-	if (maxLevel < 1) {
-	problems.add(rootObject.getPath().getObject("max_level")
-	.createProblem("Expected a value \u2265 1"));
-	}
-	});
-	
-	var optPointsPerLevel = rootObject.get("points_per_level")
-	.getSuccess() // ignore failure because this property is optional
-	.flatMap(element -> element.getAsInt()
-	.ifFailure(problems::add)
-	.getSuccess());
-	optPointsPerLevel.ifPresent(points -> {
-	if (points < 0) {
-	problems.add(rootObject.getPath().getObject("points_per_level")
-	.createProblem("Expected a value \u2265 0"));
-	}
-	});
+        // Access optional fields and validate values
+        var optSkillId = rootObject.getString("skill_id")
+                .ifFailure(problems::add)
+                .getSuccess();
 
-        var optMaxLevel = rootObject.get("max_level")
+        var optMaxLevelTmp = rootObject.get("max_level")
                 .getSuccess() // optional
                 .flatMap(element -> element.getAsInt()
                                 .ifFailure(problems::add)
-                                .getSuccess())
-                .orElse(Integer.MAX_VALUE);
+                                .getSuccess());
+        optMaxLevelTmp.ifPresent(maxLevel -> {
+            if (maxLevel < 1) {
+                problems.add(rootObject.getPath().getObject("max_level")
+                                .createProblem("Expected a value \u2265 1"));
+            }
+        });
+        int optMaxLevel = optMaxLevelTmp.orElse(Integer.MAX_VALUE);
 
-        var optPointsPerLevel = rootObject.get("points_per_level")
+        var optPointsPerLevelTmp = rootObject.get("points_per_level")
                 .getSuccess() // optional
                 .flatMap(element -> element.getAsInt()
                                 .ifFailure(problems::add)
-                                .getSuccess())
-                .orElse(0);
+                                .getSuccess());
+        optPointsPerLevelTmp.ifPresent(points -> {
+            if (points < 0) {
+                problems.add(rootObject.getPath().getObject("points_per_level")
+                                .createProblem("Expected a value \u2265 0"));
+            }
+        });
+        int optPointsPerLevel = optPointsPerLevelTmp.orElse(0);
 
         if (problems.isEmpty()) {
-                return Result.success(new PerLevelRewardsReward(levelRewards,
-                                optSkillId.orElse(null),
-                                optMaxLevel,
-                                optPointsPerLevel));
+            return Result.success(new PerLevelRewardsReward(levelRewards,
+                            optSkillId.orElse(null),
+                            optMaxLevel,
+                            optPointsPerLevel));
         } else {
-                return Result.failure(Problem.combine(problems));
+            return Result.failure(Problem.combine(problems));
         }
-	}
+    }
 
-	@Override
-	public void update(RewardUpdateContext context) {
-	for (var entry : levelRewards.entrySet()) {
-		int level = entry.getKey();
-		int count = context.getCount() >= level ? 1 : 0;
-		for (var reward : entry.getValue()) {
-		reward.instance().update(new RewardUpdateContextImpl(context.getPlayer(), count, context.isAction()));
-		}
-	}
-	}
+    @Override
+    public void update(RewardUpdateContext context) {
+        for (var entry : levelRewards.entrySet()) {
+            int level = entry.getKey();
+            int count = context.getCount() >= level ? 1 : 0;
+            for (var reward : entry.getValue()) {
+                reward.instance().update(new RewardUpdateContextImpl(context.getPlayer(), count, context.isAction()));
+            }
+        }
+    }
 
-	@Override
-	public void dispose(RewardDisposeContext context) {
-	var disposeContext = new DisposeContext(context.getServer());
-	for (var rewardList : levelRewards.values()) {
-		for (var reward : rewardList) {
-		reward.dispose(disposeContext);
-		}
-	}
-	}
+    @Override
+    public void dispose(RewardDisposeContext context) {
+        var disposeContext = new DisposeContext(context.getServer());
+        for (var rewardList : levelRewards.values()) {
+            for (var reward : rewardList) {
+                reward.dispose(disposeContext);
+            }
+        }
+    }
 }

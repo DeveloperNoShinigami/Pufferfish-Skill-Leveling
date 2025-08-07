23ec5dd7dc962dad378eba1535ed00af48ede391
Blue Lotus Coding <62197849+DeveloperNoShinigami@users.noreply.github.com>
2025-08-07 02:02:53 -0500

    Simplify client setup and packet registration

--- Fabric/src/main/java/net/puffish/skillsmod/main/FabricClientMain.java
+++ Fabric/src/main/java/net/puffish/skillsmod/main/FabricClientMain.java
@@ -2,18 +2,10 @@ package net.puffish.skillsmod.main;
 
 import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.client.option.KeyBinding;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 import net.puffish.skillsmod.client.SkillsClientMod;
-import net.puffish.skillsmod.client.event.ClientEventListener;
-import net.puffish.skillsmod.client.event.ClientEventReceiver;
-import net.puffish.skillsmod.client.keybinding.KeyBindingHandler;
-import net.puffish.skillsmod.client.keybinding.KeyBindingReceiver;
 import net.puffish.skillsmod.client.network.ClientPacketHandler;
 import net.puffish.skillsmod.client.network.ClientPacketSender;
 import net.puffish.skillsmod.client.setup.ClientRegistrar;
@@ -24,15 +16,13 @@ import java.util.function.Function;
 
 public class FabricClientMain implements ClientModInitializer {
 
-	@Override
-	public void onInitializeClient() {
-		SkillsClientMod.setup(
-				new ClientRegistrarImpl(),
-				new ClientEventReceiverImpl(),
-				new KeyBindingReceiverImpl(),
-				new ClientPacketSenderImpl()
-		);
-	}
+        @Override
+        public void onInitializeClient() {
+                SkillsClientMod.setup(
+                                new ClientRegistrarImpl(),
+                                new ClientPacketSenderImpl()
+                );
+        }
 
 	private static class ClientRegistrarImpl implements ClientRegistrar {
 		@Override
@@ -50,35 +40,12 @@ public class FabricClientMain implements ClientModInitializer {
 		public void registerOutPacket(Identifier id) { }
 	}
 
-	private static class ClientEventReceiverImpl implements ClientEventReceiver {
-		@Override
-		public void registerListener(ClientEventListener eventListener) {
-			ClientPlayConnectionEvents.JOIN.register(
-					(handler, sender, client) -> eventListener.onPlayerJoin()
-			);
-		}
-	}
-
-	private static class KeyBindingReceiverImpl implements KeyBindingReceiver {
-		@Override
-		public void registerKeyBinding(KeyBinding keyBinding, KeyBindingHandler handler) {
-			ClientTickEvents.END_CLIENT_TICK.register(
-					client -> {
-						if (keyBinding.wasPressed()) {
-							handler.handle();
-						}
-					}
-			);
-			KeyBindingHelper.registerKeyBinding(keyBinding);
-		}
-	}
-
-	private static class ClientPacketSenderImpl implements ClientPacketSender {
-		@Override
-		public void send(OutPacket packet) {
-			var buf = new PacketByteBuf(Unpooled.buffer());
-			packet.write(buf);
-			ClientPlayNetworking.send(packet.getId(), buf);
-		}
-	}
+        private static class ClientPacketSenderImpl implements ClientPacketSender {
+                @Override
+                public void send(OutPacket packet) {
+                        var buf = new PacketByteBuf(Unpooled.buffer());
+                        packet.write(buf);
+                        ClientPlayNetworking.send(packet.getId(), buf);
+                }
+        }
 }
